f239a2bb76edb00ccce60c249ef18c72aeafcc74
huali9 <huliu@redhat.com>
2024-10-17 13:33:28 +0800

    test multiple domains

--- ci-operator/step-registry/aws/provision/vpc/shared/aws-provision-vpc-shared-commands.sh
+++ ci-operator/step-registry/aws/provision/vpc/shared/aws-provision-vpc-shared-commands.sh
@@ -351,7 +351,7 @@ Resources:
     Type: AWS::EC2::DHCPOptions
     Condition: DoDhcp
     Properties:
-        DomainName: HUALI-Qe.example.com
+        DomainName: qe.devcluster.openshift.com HUALI-Qe.example.com
         DomainNameServers:
           - AmazonProvidedDNS
   VPCDHCPOptionsAssociation:
